7f057afdbca215e9739e3b3b8d06fa6573fbb8b7
lokilow <lokilow@pm.me>
2024-07-16 17:33:41 -0400

    iterators
    love me some fp

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -1,4 +1,9 @@
 fn factorial(num: u64) -> u64 {
+    if num == 0 {
+        1
+    } else {
+        (1..num + 1).reduce(|acc, e| acc * e).unwrap()
+    }
     // TODO: Complete this function to return the factorial of `num` which is
     // defined as `1 * 2 * 3 * … * num`.
     // https://en.wikipedia.org/wiki/Factorial
